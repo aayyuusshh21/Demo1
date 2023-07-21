906e838718ec50d8da4c09f3157801bdd5ca9b51
aayyuusshh21 <aayyuusshh21@gmail.com>
2023-07-21 11:37:43 +0530

    Changed Button

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,14 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export const Counter=()=>{
-    const[count,setCount]=useState(0);
-    return(
-        <div>
-            <h1>count{count}</h1>
-            <button onClick={()=>{
-                setCount(count+1)
-            }}></button>
-        </div>
-    )
-}
+export const Counter = () => {
+  const [count, setCount] = useState(0);
+  function add() {
+    setCount(count + 1);
+  }
+  return (
+    <div>
+      <h1>count:- {count}</h1>
+      <button onClick={add}></button>
+    </div>
+  );
+};
